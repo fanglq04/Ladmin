8b24020a2c21f2dc487c1baea815ae34948ee13c
270494194@qq.com <JiafangWang>
2019-04-25 21:10:49 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ----------------------------------------------
+ * Desc:
+ * User: Jiafang.Wang（270494194@qq.com）
+ * Date: 2019\4\25 0025 21:08
+ * File: Test.php
+ * 专注于企业网站开发、内部系统，小程序、微信公众号，
+ * 为小微企业提供一站式技术服务，建立信任基础上长期合作。
+ * ----------------------------------------------
+ */
+namespace WangJF\Ladmin;
+
+class Test
+{
+    public function index()
+    {
+        echo 'my name is wangjifang'.PHP_EOL;
+    }
+}
