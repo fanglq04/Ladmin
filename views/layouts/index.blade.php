0a423638de6fe2c8babb26ececb8eb7be4929ed9
270494194@qq.com <JiafangWang>
2019-05-14 16:23:17 +0800

    重新定义静态资源路径

--- views/layouts/index.blade.php
+++ views/layouts/index.blade.php
@@ -3,60 +3,30 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Jiafang.Wang" content="王家访,270494194@qq.com">
     <title>{{config('admin.title')}}</title>
-    <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
-
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/ionicons/2.0.1/css/ionicons.min.css') }}">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/font-awesome/css/font-awesome.min.css") }}">
-
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/AdminLTE/dist/css/skins/" . config('admin.skin') .".min.css") }}">
-
-    <!-- Datatables style -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/datatables/jquery.dataTables.min.css") }}">
-
-    <!-- Datatables Boostrap -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/datatables/dataTables.bootstrap.css') }}">
-
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/AdminLTE/plugins/iCheck/square/red.css') }}">
-
-    <!-- select2 -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/AdminLTE/plugins/select2/select2.min.css') }}">
-
-    <!-- FilInput -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/bootstrap-fileinput/css/fileinput.min.css') }}">
-
-    <!-- load -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/load/load.css') }}">
-
-    <!-- nestable -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/nestable/nestable.css") }}">
-
-    <!-- toastr -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/toastr/build/toastr.min.css') }}">
-
-    <!-- nprogress -->
-    <link rel="stylesheet" href="{{ asset('/packages/admin/nprogress/nprogress.css') }}">
-    <!-- AdminLTE -->
-    <link rel="stylesheet" href="{{ asset("/packages/admin/AdminLTE/dist/css/AdminLTE.min.css") }}">
-
+    <link rel="stylesheet" href="{{ asset("/library/bootstrap/css/bootstrap.min.css") }}">
+    <link rel="stylesheet" href="{{ asset('/library/ionicons/2.0.1/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset("/library/font-awesome/css/font-awesome.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("/library/AdminLTE/css/skins/" . config('admin.skin') .".min.css") }}">
+    <link rel="stylesheet" href="{{ asset("/library/datatables/jquery.dataTables.min.css") }}">
+    <link rel="stylesheet" href="{{ asset('/library/datatables/dataTables.bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('/library/iCheck/square/red.css') }}">
+    <link rel="stylesheet" href="{{ asset('/library/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/library/bootstrap-fileinput/css/fileinput.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/library/load/load.css') }}">
+    <link rel="stylesheet" href="{{ asset("/library/nestable/nestable.css") }}">
+    <link rel="stylesheet" href="{{ asset('/library/toastr/build/toastr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/library/nprogress/nprogress.css') }}">
+    <link rel="stylesheet" href="{{ asset("/library/AdminLTE/css/AdminLTE.min.css") }}">
     @stack('css')
-
-    <!-- REQUIRED JS SCRIPTS -->
-    <script src="{{ asset ("/packages/admin/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js") }}"></script>
-    <script src="{{ asset ("/packages/admin/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
-    <script src="{{ asset ("/packages/admin/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js") }}"></script>
-    <script src="{{ asset ("/packages/admin/AdminLTE/dist/js/app.min.js") }}"></script>
-    <script src="{{ asset ("/packages/admin/jquery-pjax/jquery.pjax.js") }}"></script>
-    <script src="{{ asset('/packages/admin/nprogress/nprogress.js') }}"></script>
+    <script src="{{ asset ("/library/jQuery/jQuery-2.1.4.min.js") }}"></script>
+    <script src="{{ asset ("/library/bootstrap/js/bootstrap.min.js") }}"></script>
+    <script src="{{ asset ("/library/slimScroll/jquery.slimscroll.min.js") }}"></script>
+    <script src="{{ asset ("/library/AdminLTE/js/app.min.js") }}"></script>
+    <script src="{{ asset ("/library/jquery-pjax/jquery.pjax.js") }}"></script>
+    <script src="{{ asset('/library/nprogress/nprogress.js') }}"></script>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -97,30 +67,21 @@
 <!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
-<script src="{{ asset ("/packages/admin/AdminLTE/plugins/chartjs/Chart.min.js") }}"></script>
-<script src="{{ asset ("/packages/admin/nestable/jquery.nestable.js") }}"></script>
-<script src="{{ asset('/packages/admin/AdminLTE/plugins/iCheck/icheck.min.js') }}"></script>
-<script src="{{ asset('/packages/admin/AdminLTE/plugins/select2/select2.full.min.js') }}"></script>
-<script src="{{ asset('/packages/admin/AdminLTE/plugins/select2/i18n/zh-CN.js') }}"></script>
-<script src="{{ asset('/packages/admin/AdminLTE/plugins/layer/src/layer.js') }}"></script>
-<script src="{{ asset('/packages/admin/toastr/build/toastr.min.js') }}"></script>
-<script src="{{ asset('/packages/admin/helpers/helpers.js') }}"></script>
+<script src="{{ asset ("/library/chartjs/Chart.min.js") }}"></script>
+<script src="{{ asset ("/library/estable/jquery.nestable.js") }}"></script>
+<script src="{{ asset('/library/iCheck/icheck.min.js') }}"></script>
+<script src="{{ asset('/library/select2/select2.full.min.js') }}"></script>
+<script src="{{ asset('/library/select2/i18n/zh-CN.js') }}"></script>
+<script src="{{ asset('/library/layer/layer.js') }}"></script>
+<script src="{{ asset('/library/toastr/toastr.min.js') }}"></script>
 <script>
     $(document).ready(function(){
-        /**
-         * iCheck init
-         */
         $('.icheck').iCheck({
             checkboxClass: 'icheckbox_square-red',
             radioClass: 'iradio_square-red',
             increaseArea: '20%' // optional
         });
-
-        /**
-         * select2 init
-         */
         $(".select2").select2();
-
         $('table').delegate('.btn-del','click',function(){
             $('.deleteForm').attr('action',$(this).attr('data-href'));
             $('#modal-delete').modal();
